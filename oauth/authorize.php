60c5f2946b2e5201242d85b220b0f4f53ec70c4c
2m0a0x0 <selmaniesner@outlook.de>
2024-05-06 02:48:03 +0200

    LDAP überall entfernt

--- oauth/authorize.php
+++ oauth/authorize.php
@@ -46,7 +46,7 @@ else if (empty($_POST)) {
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="./style.css">
-        <title>Mattermost - LDAP Authorization</title>
+        <title>Mattermost - HKA Authorization</title>
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
         integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
